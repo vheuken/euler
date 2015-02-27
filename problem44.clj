9f7d9374079ba32b14fd6040b10755bb61a3be44
Vincent Heuken <me@vincentheuken.com>
2015-02-27 08:01:36 -0800

    cleaned up more!

--- problem44.clj
+++ problem44.clj
@@ -1,12 +1,12 @@
 (defn p [n] (/ (* n (- (* 3 n) 1)) 2))
 
 (defn pentagonal? [n]
-  (loop [n n m 0]
+  (loop [m 0]
     (cond
       (< m n)
         (if (== n (p m))
           true
-          (recur n (inc m)))
+          (recur (inc m)))
       :else false)))
 
 (println (pentagonal? 4))
