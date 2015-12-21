3bc55df876f50cf2f6703326d4fcbdd833de4c9e
Vincent Heuken <me@vincentheuken.com>
2015-12-20 16:28:47 -0800

    skimmed more time off 44...

--- problem44.clj
+++ problem44.clj
@@ -5,15 +5,35 @@
   ([n] (lazy-seq (cons (p n) (pentagonals (inc n))))))
 
 (defn pentagonal? [n lower-bound]
-  (loop [i lower-bound]
-    (let [pi (p i)]
-      (cond
-        (> n pi)
-          (recur (inc i))
-        (= n pi)
-          true
-        :else
-          false))))
+  (let [f (first  (pentagonals lower-bound))
+        s (second (pentagonals lower-bound))]
+    (loop [i (cond
+               (and (odd? f) (odd? s))
+                 (if (odd? n)
+                   lower-bound
+                   (+ 2 lower-bound))
+               (and (odd? f) (even? s))
+                 (if (odd? n)
+                   (- lower-bound 1)
+                   (+ 1 lower-bound))
+               (and (even? f) (even? s))
+                 (if (odd? n)
+                   (+ 2 lower-bound)
+                   lower-bound)
+               (and (even? f) (odd? s))
+                 (if (odd? n)
+                   (+ 1 lower-bound)
+                   (- lower-bound 1)))
+           c 0]
+      (let [pi (p i)]
+        (cond
+          (> n pi)
+            (recur (if (odd? c) (+ 3 i) (inc i))
+                   (inc c))
+          (= n pi)
+            true
+          :else
+            false)))))
 
 (defn run []
   (loop [j 1 k 1]
